fe483951dedce81a0d80cf40707fc82c099a6b3b
spark-01 <>
2024-05-23 13:09:35 +0900

    API

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weatherImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func btnReload(_ sender: Any) {
+        setWeatherType()
+    }
+    
+    func setWeatherType() {
+        let fetchWeatherString = YumemiWeather.fetchWeatherCondition()
+        
+        var weatherName = "sunny"
+        var tintColor = UIColor.red
+        
+        switch fetchWeatherString {
+        case "sunny":
+            weatherName = "sunny"
+            tintColor = UIColor.red
+        case "cloudy":
+            weatherName = "cloudy"
+            tintColor = UIColor.gray
+        case "rainy":
+            weatherName = "rainy"
+            tintColor = UIColor.blue
+        default:
+            break
+        }
+
+        weatherImageView.image = UIImage(named: weatherName)
+        weatherImageView.tintColor = tintColor
+        
+    }
 
 }
 
